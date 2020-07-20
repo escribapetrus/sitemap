26f27c465a2fb1421443483fbac8b6c8013fe223
escribapetrus <schreiber.mmb@gmail.com>
2020-07-20 12:54:10 -0300

    make url filter for status 200

--- lib/get_sitemap.ex
+++ lib/get_sitemap.ex
@@ -12,6 +12,7 @@ defmodule Sitemap.GetSitemap do
     end
   end
 
+  #enter and xml file containing urls as <loc>http://example.com</loc> and the program will make a list of urls
   def get_urls_from_sitemap(xml) do
     xml
     |> stream_tags(:loc)
@@ -22,4 +23,15 @@ defmodule Sitemap.GetSitemap do
     |> Enum.map(fn x -> String.trim(x) end)
   end
 
+  def filter_by_status(urls) do
+    Enum.filter(urls, fn x -> validate_url_status(x) end)
+  end
+
+  #function checks whether url request status is 200 or not, returning true or false
+  def validate_url_status(url) do
+    case HTTPoison.get(url) do
+      {:ok, %HTTPoison.Response{status_code: 200}} -> true
+      _ -> false
+    end
+  end
 end
